30d69fd7c785d9e451885e67aba1adfa3a48067f
Pablo <pabloridolfi@gmail.com>
2018-01-29 11:03:58 -0300

    test changes
    Signed-off-by: Pablo <pabloridolfi@gmail.com>

--- src/program.py
+++ src/program.py
@@ -12,8 +12,8 @@ wiringpi.pinMode(21, 1)
 
 while True:
     wiringpi.digitalWrite(21, 1)
-    time.sleep(0.2)
+    time.sleep(0.5)
     wiringpi.digitalWrite(21, 0)
-    time.sleep(0.2)
+    time.sleep(0.5)
     n.notify("WATCHDOG=1")
 
