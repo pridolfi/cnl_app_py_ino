666b894a6d6ec3676056736788dd1b457a5bc673
Pablo Ridolfi <pridolfi@users.noreply.github.com>
2019-03-17 00:28:51 -0300

    heartbeat

--- ino/cnl_app.ino
+++ ino/cnl_app.ino
@@ -9,8 +9,12 @@ void setup()
 
 void loop() 
 {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
