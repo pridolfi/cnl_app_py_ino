ef4845234bdd38a8f5e1cba3979192817dfcf652
Pablo Ridolfi <pridolfi@users.noreply.github.com>
2019-04-29 14:46:30 -0300

    Update program.py

--- src/program.py
+++ src/program.py
@@ -12,7 +12,7 @@ wiringpi.pinMode(20, 1)
 
 while True:
     wiringpi.digitalWrite(20, 1)
-    time.sleep(2)
+    time.sleep(0.5)
     wiringpi.digitalWrite(20, 0)
-    time.sleep(2)
+    time.sleep(0.5)
     n.notify("WATCHDOG=1")
